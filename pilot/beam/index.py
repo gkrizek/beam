dc43418dfa26d9ca84e1a38db853cd50f14ba95a
Graham Krizek <gkrizek@nodesource.com>
2018-09-06 21:18:34 -0500

    Get Click working..

--- pilot/beam/index.py
+++ pilot/beam/index.py
@@ -5,30 +5,27 @@ import click
 @click.group()
 def main():
     """
-    Beam Pilot
+    Beam Pilot - Cosmos Validator Manager
     """
 
-@main.command('init', short_help="Initialize Beam Pilot")
-@main.command('start', short_help="Run Beam Pilot")
-@main.command('status', short_help="Check status of Beam Pilot")
-@main.command('version', short_help="Check version of Beam Pilot")
-@click.option('--config', help="Configuration file to use. [Default is ~/.beam/config.toml]")
 
-def init(config):
-    print(config)
+@main.command('init', short_help="initialize")
+def init():
     click.echo('init command')
 
 
+@main.command('start', short_help="start the agent")
+@click.option('--config', default="~/.beam/config.toml", help="Configuration file to use. [Default is ~/.beam/config.toml]", metavar='<FILE>')
 def start(config):
     print(config)
     click.echo('start command')
 
 
-def status(config):
-    print(config)
+@main.command('status', short_help="check agent status")
+def status():
     click.echo('status command')
 
 
-def version(config):
-    print(config)
+@main.command('version', short_help="check agent version")
+def version():
     click.echo('version command')
